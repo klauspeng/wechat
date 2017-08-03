a05dc019440f0c824957df24dfc37853de694b3f
klaus <2511634167@qq.com>
2017-08-04 07:57:58 +0800

    自动获取access_token

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -9,5 +9,20 @@ use think\Controller;
  */
 class Admin extends Controller
 {
-
+    // 初始化
+    protected function _initialize()
+    {
+        // 自动获取access_token
+        if (!cache('token')) {
+            // 获取账号密码
+            $appId  = cache('appid');
+            $secret = cache('secret');
+            // 获取token
+            $token = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential
+                &appid={$appId}&secret={$secret}");
+            $token = json_decode($token, true);
+            // 保存
+            cache('token', $token['access_token'], $token['expires_in']);
+        }
+    }
 }
